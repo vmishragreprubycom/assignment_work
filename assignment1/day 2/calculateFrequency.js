21018d42d5a4b5d9c67d1aded392bbfebdedc096
Vinay Mishra <VMISHRA@GREPRUBY.COM>
2017-06-14 19:59:48 +0530

    suggested changes in assignment1

--- assignment1/day 2/calculateFrequency.js
+++ assignment1/day 2/calculateFrequency.js
@@ -1,27 +1,20 @@
 // Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
-function calculateFrequency(string) 
-{
-  // Write your code here
-  var output=[];
- // var a=string.split('');
-  var a=Array.from(string);
-  console.log(a);
-  for(i=0;i<a.length;i++)
-   {
-      if(a[i].charCodeAt(0)>96 && a[i].charCodeAt(0)<123)
-       {
-         
-          if(output[a[i]])
-            {
-              output[a[i]]++;
+function calculateFrequency(string) {
+    // Write your code here
+    var output = [];
+    // var a=string.split('');
+    var array = Array.from(string);
+    //console.log(array);
+    for (i = 0; i < array.length; i++) {
+        if (array[i].charCodeAt(0) > 96 && array[i].charCodeAt(0) < 123) {
+
+            if (output[array[i]]) {
+                output[array[i]]++;
+            } else {
+                output[array[i]] = 1;
             }
-            else
-              {
-                output[a[i]]=0;
-                 output[a[i]]++;
-              }
-       }
+        }
     }
-  console.log(output);
-  return output;
+    //console.log(output);
+    return output;
 }
--- assignment1/day 2/unflatten.js
+++ assignment1/day 2/unflatten.js
@@ -1,28 +1,24 @@
 // Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
-function unflatten(flatObject) 
-{
-  // Write your code here
-  var output = {};
+function unflatten(flatObject) {
+    // Write your code here
+    var output = {};
 
-  for (var key in flatObject)
-  {
-    //alert(key.split('.'));
-    makeUnflatten(output, key.split('.'), flatObject[key]);
-  }
+    for (var key in flatObject) {
+        //alert(key.split('.'));
+        makeUnflatten(output, key.split('.'), flatObject[key]);
+    }
 
-  return output;
+    return output;
 }
 
-function makeUnflatten (output, parts, value)
-{
-  var lastKeyIndex = parts.length - 1;
-  for(var i = 0; i < lastKeyIndex; ++i)
-  {
-    var key = parts[i];
-    if (!(key in output)) {
-      output[key] = /^\d+$/.test(parts[i + 1]) ? [] : {};
+function makeUnflatten(output, parts, value) {
+    var lastKeyIndex = parts.length - 1;
+    for (var i = 0; i < lastKeyIndex; ++i) {
+        var key = parts[i];
+        if (!(key in output)) {
+            output[key] = /^\d+$/.test(parts[i + 1]) ? [] : {};
+        }
+        output = output[key];
     }
-    output = output[key];
-  }
-  output[parts[lastKeyIndex]] = value;
+    output[parts[lastKeyIndex]] = value;
 }
