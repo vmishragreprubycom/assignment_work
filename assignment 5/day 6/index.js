0ab0bd93bdd1dedb976b6868a0b8bdc8d01ee75e
Vinay Mishra <VMISHRA@GREPRUBY.COM>
2017-06-20 15:25:30 +0530

    suggested changes in index.js

---
+++ assignment 5/day 6/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const files = ["./files/mongodb.txt",
+  "./files/expressjs.txt",
+  "./files/angularjs.txt",
+  "./files/nodejs.txt",
+  "./files/fullStack.txt"
+];
+
+function readFile(file, index) {
+  fs.readFile(file, 'utf8', function(
+    err, data) {
+    if (err) console.log(err);
+    console.log(data);
+    index++;
+    if (index < files.length)
+      readFile(files[index], index);
+  });
+}
+
+readFile(files[0], 0);
