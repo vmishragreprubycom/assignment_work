be56320668ae72531fc27190387ceaca514121f0
Vinay Mishra <VMISHRA@GREPRUBY.COM>
2017-06-20 17:13:22 +0530

    suggested changes done in index.js

--- assignment 6/day 7/index.js
+++ assignment 6/day 7/index.js
@@ -1,29 +1,37 @@
 const fs = require('fs');
 
 function readFile(file) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(file, 'utf8', function(err, data) {
-      if (err) return reject(err);
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
       resolve(data);
     })
   });
 }
 
-readFile("./files/mongodb.txt").then(function(data) {
-  console.log(data);
-  return readFile("./files/expressjs.txt");
-}).then(function(data) {
-  console.log(data);
-  return readFile("./files/angularjs.txt");
-}).then(function(data) {
-  console.log(data);
-  return readFile("./files/nodejs.txt");
-}).then(function(data) {
-  console.log(data);
-  return readFile("./files/fullStack.txt");
-}).then(function(data) {
-  console.log(data);
-  return readFile("./files/fullStack.txt");
-}).catch(function(err) {
-  console.log(err);
-});
+readFile("./files/mongodb.txt")
+  .then((result) => {
+    console.log(result);
+    return readFile("./files/expressjs.txt");
+  })
+  .then((result) => {
+    console.log(result);
+    return readFile("./files/angularjs.txt");
+  })
+  .then((result) => {
+    console.log(result);
+    return readFile("./files/nodejs.txt");
+  })
+  .then((result) => {
+    console.log(result);
+    return readFile("./files/fullStack.txt");
+  })
+  .then((result) => {
+    console.log(result);
+    return readFile("./files/fullStack.txt");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
